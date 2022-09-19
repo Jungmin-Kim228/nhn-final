0ac323e1fb4afe74213f6cd50def4bda9f495cf9
Moomin <moominK228@gmail.com>
2022-09-19 12:45:11 +0900

    Q1-1) JPA Entity 개발

--- final_exam-master/src/main/java/com/springtour/hotel/domain/Hotel.java
+++ final_exam-master/src/main/java/com/springtour/hotel/domain/Hotel.java
@@ -1,4 +1,21 @@
 package com.springtour.hotel.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "Hotels")
+@Entity
 public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hotel_id")
+    private Long id;
+
+    private String name;
+
 }
--- final_exam-master/src/main/java/com/springtour/hotel/domain/Room.java
+++ final_exam-master/src/main/java/com/springtour/hotel/domain/Room.java
@@ -1,4 +1,38 @@
 package com.springtour.hotel.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Table(name = "Rooms")
+@Entity
 public class Room {
+
+    @Id
+    @Column(name = "room_id")
+    private Long id;
+
+    @JoinColumn(name = "hotel_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Hotel hotel;
+
+    private String name;
+
+    private Integer capacity;
+
+    private Integer floor;
+
+    @Column(name = "bathtub_flag")
+    private boolean bathtubFlag;
+
+    @Column(name = "view_type")
+    private ViewType viewType;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
