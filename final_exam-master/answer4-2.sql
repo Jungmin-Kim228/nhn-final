58e744fec026770deeda01783b90bfd9d53daa72
Moomin <moominK228@gmail.com>
2022-09-19 18:00:23 +0900

    Q4-2) 예약정보(Books) 테이블 설계

---
+++ final_exam-master/answer4-2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `final_exam9`.`Books` (
+     `book_id`	BIGINT	NOT NULL AUTO_INCREMENT,
+     `room_id`	BIGINT	NOT NULL,
+     `user_id`	BIGINT	NOT NULL,
+     `check_in`	DATE	NOT NULL,
+     `check_out`	DATE	NOT NULL,
+     PRIMARY KEY (`book_id`),
+    CONSTRAINT `fk_Books_Rooms`
+    FOREIGN KEY (`room_id`)
+    REFERENCES `final_exam9`.`Rooms` (`room_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
